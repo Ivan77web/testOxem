75d4c33d4262cef556ff79a5a5e92c0427d7f645
Ivan77web <ivanshestopalov39@gmail.com>
2022-09-30 20:49:02 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import cl from "./App.module.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className={cl.test}>
+        Hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
