2ef1440ce3e71ba3134a45fd5caa3a6b096bb7c1
Ivan77web <ivanshestopalov39@gmail.com>
2022-10-01 15:57:27 +0300

    added my inputs

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,55 @@
+import { useState } from "react";
+import MyInput from "../ui/myInput/MyInput";
+import MyInputFirstPayment from "../ui/myInputFirstPayment/MyInputFirstPayment";
 import cl from "./App.module.css"
 
 function App() {
+  const [allPayment, setAllPayment] = useState(3300000);
+  const [percentPayment, setPercentPayment] = useState(13);
+  const [firstPayment, setFirstPayment] = useState(429000);
+  const [period, setPeriod] = useState(60)
+
   return (
-    <div className="App">
-      <div className={cl.test}>
-        Hello
+    <div className={cl.app}>
+      <div className={cl.intro}>
+        <h1>Рассчитайте стоимость <br /> автомобиля в лизинг</h1>
+
+        <div className={cl.inputs}>
+          <div className={cl.input}>
+            <MyInput
+              intro="Стоимость автомобиля"
+              symbol="&#8381;"
+              start="1000000"
+              end="6000000"
+              value={allPayment}
+              setValue={setAllPayment}
+            />
+          </div>
+
+          <div className={cl.input}>
+            <MyInputFirstPayment
+              intro="Первоначальный взнос"
+              start="10"
+              end="60"
+              value={percentPayment}
+              setValue={setPercentPayment}
+              firstPayment={firstPayment}
+              setFirstPayment={setFirstPayment}
+              allPayment={allPayment}
+            />
+          </div>
+
+          <div className={cl.input}>
+            <MyInput
+              intro="Срок лизинга"
+              symbol="мес."
+              start="1"
+              end="60"
+              value={period}
+              setValue={setPeriod}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ui/myInput/MyInput.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useRef, useState } from "react";
+import cl from "./MyInput.module.css"
+
+export default function MyInput({ 
+    intro, 
+    symbol, 
+    start, 
+    end, 
+    value,
+    setValue,
+}) {
+    const line = useRef(null);
+    const slider = useRef(null);
+    const myInput = useRef(null)
+    const activeLine = useRef(null)
+    
+    const [percentOfLine, setPercentOfLine] = useState((Number(value) * 100) / (Number(end) - Number(start)))
+    const [leftOfLine, setLeftOfLine] = useState(percentOfLine / 100 * 363);
+    const [error, setError] = useState(false);
+
+    const changeLeftOfLine = (value) => {
+        let valueLeft = value;
+
+        if (!!Number(valueLeft) || valueLeft === "") {
+            if (Number(valueLeft) < Number(start)) {
+                setValue(valueLeft)
+                setLeftOfLine(0)
+            } else if (Number(valueLeft) > Number(end)) {
+                setValue(valueLeft)
+                setLeftOfLine(363)
+            } else {
+                setValue(valueLeft)
+                setLeftOfLine((Number(valueLeft) * 100) / (Number(end) - Number(start)))
+            }
+        }
+    }
+
+    const startDnD = (event) => {
+        event.preventDefault();
+
+        let shiftX = event.clientX - slider.current.getBoundingClientRect().left;
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+
+        function onMouseMove(event) {
+            let newLeft = event.clientX - shiftX - line.current.getBoundingClientRect().left;
+            let rightEdge = line.current.offsetWidth - slider.current.offsetWidth;
+
+            if (newLeft < 0) {
+                newLeft = 0;
+            } else if (newLeft > rightEdge) {
+                newLeft = rightEdge;
+            }
+
+            setLeftOfLine(newLeft)
+            setValue(Math.round((((Number(newLeft) * 100) / 36300) * (Number(end) - Number(start))) + Number(start)));
+        }
+
+        function onMouseUp() {
+            document.removeEventListener('mouseup', onMouseUp);
+            document.removeEventListener('mousemove', onMouseMove);
+        }
+    };
+
+    function ondragstart() {
+        return false;
+    };
+
+    useEffect(() => {
+        slider.current.style.left = leftOfLine + "px";
+        activeLine.current.style.width = leftOfLine + 'px';
+    }, [leftOfLine])
+
+    useEffect(() => {
+        (Number(value) < Number(start) || Number(value) > Number(end))
+            ?
+            setError(true)
+            :
+            setError(false)
+
+    }, [value])
+
+    return (
+        <div className={cl.myInput} ref={myInput}>
+            <p className={cl.intro}>{intro}</p>
+
+            <div className={cl.inputBlock}>
+                <input className={cl.input} value={value} onChange={(e) => changeLeftOfLine(e.target.value)} />
+
+                <div className={cl.line} ref={line}>
+                    <div className={cl.activeLine} ref={activeLine}>
+                        <div className={cl.slider} ref={slider} onMouseDown={startDnD} onDragStart={ondragstart} />
+                    </div>
+                </div>
+            </div>
+
+            <div className={error ? cl.error : cl.active}>
+                {`* Ограничение от ${start + " " + symbol} до ${end + " " + symbol}`}
+            </div>
+
+            <div className={cl.symbol}>{symbol}</div>
+        </div>
+    )
+}
